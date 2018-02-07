5b9308ab01519118cf79ff95da9d1342d8de0a3d
Shoieb Rahman <shoiebrahman@gmail.com>
2018-02-07 11:49:58 +0600

    Added domain classes

---
+++ ExamApp.Domain/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ExamApp.Domain
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
